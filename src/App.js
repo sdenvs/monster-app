0c3ec995a4f0316fdc721af4f447280697d8b6e6
naval-invn <101180425+naval-invn@users.noreply.github.com>
2024-05-20 14:47:08 +0530

    new add

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      searchText: '',
+      monsters: [
+
+      ]
+    }
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then((res) => res.json())
+      .then((users) => {
+        this.setState(() => { return { monsters: users } })
+      })
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <div>
+          <h1>Monster Rolodex</h1>
+        </div>
+        <div>
+          <input type='search' placeholder='type to search' onChange={(event) => {
+            this.setState({ searchText: event.target.value })
+          }} />
+        </div>
+        <div>
+          {this.state.monsters.filter(monst => { return monst.name.toLowerCase().includes(this.state.searchText.toLowerCase()) }).map(monster => {
+            return <h1 key={monster.id}>{monster.name}</h1>
+          })}
+        </div>
+      </div>
+    );
+  }
+
 }
 
 export default App;
